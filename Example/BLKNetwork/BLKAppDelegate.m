9e68ef29e32df34cce1daf162d77576df8eb56fd
LiuHangqi <liuhangqi@live.com>
2017-01-16 13:42:32 +0800

    commit

--- Example/BLKNetwork/BLKAppDelegate.m
+++ Example/BLKNetwork/BLKAppDelegate.m
@@ -7,8 +7,8 @@
 //
 
 #import "BLKAppDelegate.h"
-#import "AFNetworking.h"
-#import "CustomURLProtocol.h"
+
+
 
 @implementation BLKAppDelegate
 
@@ -16,7 +16,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    [NSURLProtocol registerClass:[CustomURLProtocol class]];
+
     return YES;
 }
 
--- Example/Pods/Target Support Files/BLKNetwork/BLKNetwork-umbrella.h
+++ Example/Pods/Target Support Files/BLKNetwork/BLKNetwork-umbrella.h
@@ -1,8 +1,5 @@
 #import <UIKit/UIKit.h>
 
-#import "BLKBaseRequest.h"
-#import "BLKRequestQueue.h"
-#import "BLKRequestQueueConfig.h"
 
 FOUNDATION_EXPORT double BLKNetworkVersionNumber;
 FOUNDATION_EXPORT const unsigned char BLKNetworkVersionString[];
