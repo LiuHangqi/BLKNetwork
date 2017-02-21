a28b67360da94a87bcba333aabf58e8651a331f0
liu <liu@AC960F15.ipt.aol.com>
2017-02-21 14:37:13 +0800

    修改

---
+++ BLKNetwork/Classes/BLKRequest.h
@@ -0,0 +1,9 @@
+//
+// Created by liu on 2017/2/3.
+//
+
+#import <Foundation/Foundation.h>
+#import "BLKBaseRequest.h"
+
+@interface BLKRequest : BLKBaseRequest
+@end
---
+++ BLKNetwork/Classes/BLKRequest.m
@@ -0,0 +1,11 @@
+//
+// Created by liu on 2017/2/3.
+//
+
+#import "BLKRequest.h"
+
+
+@implementation BLKRequest {
+
+}
+@end
